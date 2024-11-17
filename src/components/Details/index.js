48615096de17872c2eaa571418bdec6f964952ec
Ganesh <meetganeshbaskar@gmail.com>
2024-11-17 22:31:28 +0530

    - react-helmet added - style fix

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,4 +1,5 @@
-import React,  { useEffect }  from "react";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import HotelMapComponent from "./components/Gmap";
@@ -17,12 +18,27 @@ const HotelDetailsComponent = () => {
   }, [id, dispatch]);
 
   const hotel = hotels.length > 0 ? hotels[0] : null;
-  return( 
-  <div  className={styles.container}>
-    <HotelInfoComponent hotel={hotel}/>
-    <HotelMapComponent hotel={hotel}/>
-  </div>
-  )
+  return (
+    <>
+      <Helmet>
+        <title>{"Hotel Details - My App"}</title>
+        <meta
+          name="description"
+          content={
+            hotel
+              ? `Explore details of ${hotel.name}, including location and amenities.`
+              : "Hotel details page"
+          }
+        />
+        <meta name="keywords" content="hotel, booking, travel, accommodation" />
+        {hotel && <meta property="og:image" content={hotel.image} />}
+      </Helmet>
+      <div className={styles.container}>
+        <HotelInfoComponent hotel={hotel} />
+        <HotelMapComponent hotel={hotel} />
+      </div>
+    </>
+  );
 };
 
 export default HotelDetailsComponent;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import HomeFilter from "./components/HomeFilter";
 import HomeList from "./components/HomeList";
 import styles from "./styles.module.css";
@@ -6,6 +7,17 @@ import styles from "./styles.module.css";
 const HomeComponent = () => {
   return (
     <>
+      <Helmet>
+        <title>Hotel's List - My App</title>
+        <meta
+          name="description"
+          content="Browse through a list of hotels to find the perfect one for your stay."
+        />
+        <meta
+          name="keywords"
+          content="hotels, booking, travel, accommodation"
+        />
+      </Helmet>
       <div className={styles.container}>
         <div className={styles.header}>
           <h1>Hotel's List</h1>
